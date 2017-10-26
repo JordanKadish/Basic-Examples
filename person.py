0aedd5b828766433001a676a41c00a48c3d23a42
Jordan Kadish <jordankadish93@gmail.com>
2017-10-26 19:12:10 +0200

    pep8 is pretty annoying

---
+++ person.py
@@ -0,0 +1,27 @@
+# Jordan Kadish
+# 26/10/2017
+# Person OOP class
+# Testing out pep8 formatting
+"""Testing out this pep8 stuff. looks a bit hideous tbqh"""
+
+
+class Person:
+    """This is a generic person class. """
+
+    def __init__(self, name, mid_name, surname, age):
+        self.name = name
+        self.mid_name = mid_name
+        self.surname = surname
+        self.age = age
+
+    def __str__(self):
+        return self.name + " " + self.surname + ", " + str(self.age) + \
+            " years old"
+
+    def initials(self):
+        """returns the person's initials, separated by a comma"""
+        return self.name[0] + "." + self.mid_name[0] + "." + self.surname[0]
+
+    def happy_birthday(self):
+        """ages the person by one year"""
+        self.age += 1
